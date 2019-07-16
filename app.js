6f279f1f9af238660f4007c7c9953a94ed76cf7e
JoBeom-jin <whqjawls8@gmail.com>
2019-07-16 09:55:27 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+// app.get('/', (req, res) => res.send('Hello World!')) //
+
+
+
+app.use(express.static('public'))
+
+const path = require('path');
+const router = express.Router();
+router.get('/', function (req, res) {
+res.sendFile(path.join(__dirname + '/index.html'));
+//__dirname : It will resolve to your project folder.
+});
+
+router.get('/external', function (req, res) {
+    res.sendFile(path.join(__dirname + '/views/external.html'));
+    //__dirname : It will resolve to your project folder.
+    });
+
+app.use('/', router);
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
